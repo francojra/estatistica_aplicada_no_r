f19be22e835ce9c67297799eec453cd2d459af40
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-09-27 23:30:03 -0300

    Add files via upload

--- estatistica_aplicada_no_R.R
+++ estatistica_aplicada_no_R.R
@@ -12,3 +12,24 @@
 ### Elaboração dos mais variados tipos de gráficos e produção
 ### de relatórios automáticos e reprodutíveis;
 ### Boas práticas de programação e projetos colaborativos.
+
+# Iniciando a prática ----------------------------------------------------------------------------------------------------------------------
+
+### Operações matemáticas
+
+5 + 6
+7 / 3
+6 - 2
+5 * 9
+43 ^ 5
+
+### Salvar objetos no ambiente (enviroment)
+
+a <- 2 + 6 # salvar a operação em um objeto chamado 'a'
+a # verificar o que tem no objeto
+
+### Funções
+
+b <- c(3, 5, 6, 2) # conjunto (c) de valores no objeto b
+b
+sum(b) # função de soma
