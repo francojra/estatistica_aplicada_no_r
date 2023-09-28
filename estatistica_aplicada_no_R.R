bcfa97fd1233e6d1e436081a269356f5bf72414a
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-09-27 23:11:22 -0300

    Add files via upload

---
+++ estatistica_aplicada_no_R.R
@@ -0,0 +1,14 @@
+
+# Estatística Aplicada com R ---------------------------------------------------------------------------------------------------------------
+# Autora do script: Jeanne Franco ----------------------------------------------------------------------------------------------------------
+# Data: 27/09/23 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://www.youtube.com/watch?v=-XVQuVmlJyI ----------------------------------------------------------------------------------
+
+# O que aprender? --------------------------------------------------------------------------------------------------------------------------
+
+### Importação rápida e manipulação eficiente de diversas planilhas;
+### Manipulação de dados de planilhas e arquivos como selecionar, 
+### filtrar, criar, ordenar e sumarizar dados;
+### Elaboração dos mais variados tipos de gráficos e produção
+### de relatórios automáticos e reprodutíveis;
+### Boas práticas de programação e projetos colaborativos.
